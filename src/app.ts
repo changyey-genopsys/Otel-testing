50ad260e39482600ec0aee1ae5eafa71c14212bf
changyey-genopsys <changyey@genopsys-tech.com>
2026-06-30 08:21:11 +0000

    start elasticsearch only version

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,7 @@ import express, { type Express } from 'express';
 
 import { logger } from "./logger.js";
 
-import {
-  context,
-  trace
-} from "@opentelemetry/api";
-
 const app: Express = express();
-const tracer = trace.getTracer('service');
 
 app.get("/health", async (_req, res) => {
   logger.info(
@@ -17,29 +11,8 @@ app.get("/health", async (_req, res) => {
     },
     "Health Check"
   );
-
-  // manual trace test1
-  await tracer.startActiveSpan("service", async (span) => {
-
-    await service("health Test");
-
-    span.end();
-
-  });
+  service("testing");
   
-  // manual trace test2
-  // const childspan = tracer.startSpan("db");
-
-  // const ctx =
-  //   trace.setSpan(
-  //     context.active(),
-  //     childspan
-  //   );
-
-  // context.with(ctx, () => {
-  //   logger.info("hello");
-  // });
-
   res.json({
     status: "ok",
   });
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-import { startTelemetry } from "./telemetry.js";
-await startTelemetry();
-
 import app from "./app.js";
 import { logger } from "./logger.js";
 
 async function bootstrap() {
 
   app.listen(3000, () => {
-    // If logger triggered not in instrumentations, then span_id and trace_id not included
     logger.info(
       {
         port: 3000,
