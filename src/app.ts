a91ae73af15d2bec948cb4fb841d9f8564b28c4b
changyey-genopsys <changyey@genopsys-tech.com>
2026-06-30 03:11:50 +0000

    resource attached

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,15 @@ import express, { type Express } from 'express';
 
 import { logger } from "./logger.js";
 
-const app: Express = express();
+import {
+  context,
+  trace
+} from "@opentelemetry/api";
 
-app.get("/health", (_req, res) => {
+const app: Express = express();
+const tracer = trace.getTracer('service');
+
+app.get("/health", async (_req, res) => {
   logger.info(
     {
       module: "health",
@@ -12,10 +18,36 @@ app.get("/health", (_req, res) => {
     "Health Check"
   );
 
+  // manual trace test1
+  await tracer.startActiveSpan("service", async (span) => {
+
+    await service("health Test");
+
+    // manual trace test2
+    const childspan = tracer.startSpan("db");
+
+    const ctx =
+      trace.setSpan(
+        context.active(),
+        childspan
+      );
+
+    context.with(ctx, () => {
+      logger.info("hello");
+    });
+
+    span.end();
+
+  });
+
   res.json({
     status: "ok",
   });
 });
+
+async function service(args: String) {
+  logger.info(args);
+}
 
 // const intervalId = setInterval(() => {
 //   logger.info("This message repeats every 3 seconds." + String(Date.now()));
--- src/logger.ts
+++ src/logger.ts
@@ -16,6 +16,13 @@ import {
 
 import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 
+import { resourceFromAttributes } from "@opentelemetry/resources";
+
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from "@opentelemetry/semantic-conventions";
+
 // export function toSeverity(level: number) {
 
 //   switch (level) {
@@ -46,25 +53,28 @@ import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
 // }
 
 // Optional and only needed to see the internal diagnostic logging (during development)
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const logExporter = new OTLPLogExporter({
   url: 'http://otel-collector:4318/v1/logs',
 });
 
 const loggerProvider = new LoggerProvider({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "ts-app",
+    [ATTR_SERVICE_VERSION]: "1.0.0",
+  }),
   processors: [
     new BatchLogRecordProcessor(logExporter),
-    // ConsoleLogRecordExporter -> OTLPLogExporter
-    // SimpleLogRecordProcessor -> BatchLogRecordProcessor
+    // new SimpleLogRecordProcessor(new ConsoleLogRecordExporter()) // use for console testing
   ],
 });
-
 
 logs.setGlobalLoggerProvider(loggerProvider);
 
 // Reuse loggers where possible. getLogger() may be expensive, especially when
 // scopeAttributes are provided, so avoid calling it on hot paths.
+// In elastic mean: {scope.name:example,	scope.version:1.0.0}
 export const otelLogger = logs.getLogger('example', '1.0.0');
 
 function emitToOpenTelemetry(args: any[]) {
@@ -87,8 +97,8 @@ function emitToOpenTelemetry(args: any[]) {
 
   }
 
-  const span = trace.getSpan(context.active());
-  const ctx = span?.spanContext();
+  // const span = trace.getSpan(context.active());
+  // const ctx = span?.spanContext();
   const customContext = context.active();
   otelLogger.emit({
 
@@ -109,9 +119,11 @@ function emitToOpenTelemetry(args: any[]) {
 
 }
 
+// If only use logger no initial telemetry, then it will lost span_id and trace_id
 export const logger = pino({
   level: "info",
 
+  // if called from http route function, trace_id and span_id will duplicate in otelLogger
   mixin() {
     const span =
       trace.getSpan(context.active());
@@ -144,19 +156,3 @@ export const logger = pino({
 
   },
 });
-
-
-// logger.info("test")
-
-
-// const ctx = context.active();
-
-// console.log("Context:", ctx);
-
-// const span = trace.getSpan(ctx);
-
-// console.log("Span:", span);
-
-// if (span) {
-//     console.log("SpanContext:", span.spanContext());
-// }
